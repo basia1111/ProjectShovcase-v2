92f04e2c702533fca690cbf24230671c795cccf2
basia1111 <basia.zygilewicz@gmail.com>
2024-12-30 17:13:06 +0100

    Uncomment populate while fetching user

--- lib/user/findUser.ts
+++ lib/user/findUser.ts
@@ -2,7 +2,8 @@
 
 import connectDB from "@lib/db";
 import User from "@models/User";
-import { Project, User as UserType } from "@types";
+import Project from "@models/Project";
+import { Project as ProjectType, User as UserType } from "@types";
 
 export const findUser = async (email: string) => {
   await connectDB();
@@ -11,10 +12,10 @@ export const findUser = async (email: string) => {
     return null;
   }
 
-  const returnedUser = await User.findOne({ email }); /*
-    .populate("likedProjects", "_id, title, cover")
+  const returnedUser = await User.findOne({ email })
+    .populate("likedProjects", "_id title cover")
     .populate("followedBy", "_id name image")
-    .populate("following", "_id name image");*/
+    .populate("following", "_id name image");
 
   if (!returnedUser) return null;
 
@@ -32,7 +33,7 @@ export const findUser = async (email: string) => {
     image: item.image,
   }));
 
-  const sanitizedLikedProjects = plainUser.likedProjects.map((item: Partial<Project>) => ({
+  const sanitizedLikedProjects = plainUser.likedProjects.map((item: Partial<ProjectType>) => ({
     ...item,
     _id: item._id ? item._id.toString() : "",
   }));
